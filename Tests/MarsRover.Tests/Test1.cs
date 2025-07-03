3d714b5a97a9494aabc9e2db8bfd410fc17c8d9a
James Joubert <jamesj1992m.gm@gmail.com>
2025-07-03 18:39:05 +0200

    Setup the new .NET9 Console Application with a placeholder Test project

---
+++ Tests/MarsRover.Tests/Test1.cs
@@ -0,0 +1,12 @@
+﻿namespace MarsRover.Tests
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+        }
+    }
+}
